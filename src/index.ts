5df0bed41e135edc30864b0a8bf8658ca57bc7e3
Kristof <kristof.kovacs@telenav.com>
2021-05-13 02:14:40 +0300

    Update

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from './bnLike'
 export * from './constants'
 export * from './functions'
 
-export { BN } from 'bn.js'
+// export { BN } from 'bn.js'
 export { BigNumber } from 'bignumber.js'
